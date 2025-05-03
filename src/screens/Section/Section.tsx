f5fde7f7b6ff165b6de6ef904a43c8ceee89ed00
rahulpscraftech360 <rahul@craftech360.com>
2025-05-03 19:40:38 +0530

    image fix

--- src/screens/Section/Section.tsx
+++ src/screens/Section/Section.tsx
@@ -10,7 +10,7 @@ export const Section = (): JSX.Element => {
     >
       <div className="flex flex-col items-center justify-center gap-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[725px] px-4">
         <Card className="bg-transparent border-none shadow-none w-full">
-          <CardContent className="flex flex-col items-center gap-2 p-0">
+          <CardContent className="flex flex-col items-center gap-2 ">
             <h2 className="relative self-stretch mt-[-1.00px] [font-family:'Clash_Display-Bold',Helvetica] font-bold text-white text-5xl md:text-6xl text-center tracking-[0] leading-[67.2px]">
             Meet Your Child’s New Best Friend
             </h2>
@@ -32,16 +32,16 @@ export const Section = (): JSX.Element => {
       </div>
 
       <img
-        className="absolute w-[333px] h-[500px] top-20 left-2 object-cover"
-        alt="AI toy robot on the left"
-        src="/Puppy_01 1.png"
-      />
+  className="absolute w-[200px] h-[300px] md:w-[280px] md:h-[420px] lg:w-[333px] lg:h-[500px] top-20 left-2 object-cover mt-20 md:mt-0"
+  alt="AI toy robot on the left"
+  src="/Puppy_01 1.png"
+/>
 
-      <img
-        className="absolute w-[373px] h-[500px] top-20 right-2 object-cover"
-        alt="AI toy robot on the right"
-        src="/penquin.png"
-      />
+<img
+  className="absolute w-[220px] h-[300px] md:w-[310px] md:h-[420px] lg:w-[373px] lg:h-[500px] top-20 right-2 object-cover mt-20 md:mt-0"
+  alt="AI toy robot on the right"
+  src="/penquin.png"
+/>
     </section>
   );
 };
