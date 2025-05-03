4a7ef92f4b369641612ab84024ec95cb6ef4ffa0
rahulpscraftech360 <rahul@craftech360.com>
2025-05-03 19:41:08 +0530

    image fix

--- src/screens/Section/Section.tsx
+++ src/screens/Section/Section.tsx
@@ -10,7 +10,7 @@ export const Section = (): JSX.Element => {
     >
       <div className="flex flex-col items-center justify-center gap-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[725px] px-4">
         <Card className="bg-transparent border-none shadow-none w-full">
-          <CardContent className="flex flex-col items-center gap-2 ">
+          <CardContent className="flex flex-col items-center gap-2 p-0">
             <h2 className="relative self-stretch mt-[-1.00px] [font-family:'Clash_Display-Bold',Helvetica] font-bold text-white text-5xl md:text-6xl text-center tracking-[0] leading-[67.2px]">
             Meet Your Child’s New Best Friend
             </h2>
